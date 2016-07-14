7b04c82adba53a0d5d1e97687316d077ff8fad3e
Sergio Martinez <sergio.martinez.rodriguez@intel.com>
2016-07-14 15:30:25 +0100

    Intel hex uploader
    This project will fetch jerryscript and zephyr and compile
    them without any of them having to be in the root folder
    
    Making easy to start new projects from scratch and add extra libraries to them.
    
    Signed-off-by: Sergio Martinez <sergio.martinez.rodriguez@intel.com>

--- README.md
+++ README.md
@@ -1,1 +1,161 @@
-# ihex_uploader
+### About
+
+This folder contains files to integrate JerryScript with Zephyr RTOS to
+run on a number of supported boards (like
+[Arduino 101 / Genuino 101](https://www.arduino.cc/en/Main/ArduinoBoard101),
+[Zephyr Arduino 101](https://www.zephyrproject.org/doc/board/arduino_101.html)).
+
+### How to build
+
+#### 1. Preface
+
+1. Directory structure
+
+Assume `harmony` as the path to the projects to build.
+
+We have to fetch the different libraries and systems we are going to use.
+For that, just run:
+```
+./scripts/get-dependecies.sh
+```
+
+This will fetch everything required into the deps folder
+
+The tree would look something like this.
+
+```
+harmony
+  + src
+  + build
+  + scripts
+  + deps
+     + jerryscript
+     + zephyr-project
+     + ihex
+```
+
+2. Target boards/emulations
+
+Following Zephyr boards were tested: qemu_x86, qemy_cortex_m3, arduino_101.
+
+#### 2. Prepare Zephyr
+
+Follow [this](https://www.zephyrproject.org/doc/getting_started/getting_started.html) page to get
+the Zephyr source and configure the environment.
+
+If you just start with Zephyr, you may want to follow "Building a Sample
+Application" section in the doc above and check that you can flash your
+target board.
+
+Remember to source the Zephyr environment:
+
+```
+source ./deps/zephyr-project/zephyr-env.sh
+
+export ZEPHYR_GCC_VARIANT=zephyr
+
+export ZEPHYR_SDK_INSTALL_DIR=<sdk installation directory>
+```
+
+#### 3. Build JerryScript for Zephyr
+
+The easiest way is to build and run on a QEMU emulator:
+
+For x86 architecture:
+
+```
+make -f ./build/Makefile.zephyr BOARD=qemu_x86 qemu
+```
+
+For ARM (Cortex-M) architecture:
+
+```
+make -f ./build/Makefile.zephyr BOARD=qemu_cortex_m3 qemu
+```
+
+#### 4. Build for Arduino 101
+
+```
+# assume you are in harmony folder
+cd jerry
+make -f ./targets/zephyr/Makefile.zephyr BOARD=arduino_101_factory
+```
+
+This will generate the following libraries:
+```
+./outdir/arduino_101_factory/librelease-cp_minimal.jerry-core.a
+./outdir/arduino_101_factory/librelease-cp_minimal.jerry-libm.lib.a
+./outdir/arduino_101_factory/librelease.external-cp_minimal-entry.a
+```
+
+The final Zephyr image will be located here:
+```
+./outdir/arduino_101_factory/zephyr/zephyr.elf
+```
+
+#### 5. Flashing
+
+Details on how to flash the image can be found here:
+[Flashing image](https://www.zephyrproject.org/doc/board/arduino_101.html)
+(or similar page for other supported boards).
+
+To be able to use this demo in hardware you will need the serial console
+which will be generating output to Pins 0 & 1
+
+Some examples of building the software
+
+```
+make -f ./build/Makefile.zephyr clean
+```
+
+- Not using a Jtag and having a factory stock Arduino 101.
+
+```
+make -f ./build/Makefile.zephyr BOARD=arduino_101_factory
+```
+
+Follow the Zephyr instructions to flash using the dfu-util command.
+
+
+- Using JTAG
+
+There is a helper function to flash using the JTAG and Flywatter2
+
+![alt tag](docs/arduino_101.jpg?raw=true "Example")
+```
+make -f ./build/Makefile.zephyr BOARD=arduino_101_factory flash
+
+```
+
+#### 6. Serial terminal
+
+Test a command line in a serial terminal.
+
+
+You should see something similar to this:
+```
+Jerry Compilation May 26 2016 13:37:50
+js>
+```
+
+
+Run the example javascript command test function
+```
+js> test
+Script [var test=0; for (t=100; t<1000; t++) test+=t; print ('Hi JS World! '+test);]
+Hi JS World! 494550
+```
+
+
+Try more complex functions:
+```
+js>function hello(t){t=t*10;return t}; print("result"+hello(10.5));
+```
+
+
+Help will provide a list of commands
+```
+> help
+```
+
+This program, is built in top of the Zephyr command line, so there is a limit of 10 spaces.
---
+++ src/main-zephyr.c
@@ -0,0 +1,177 @@
+/* Copyright 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <zephyr.h>
+#include <misc/printk.h>
+#include <misc/shell.h>
+
+#include "jerry-api.h"
+
+#include "uart-uploader.h"
+
+//#define CONFIG_USE_JS_SHELL
+
+#if defined (CONFIG_STDOUT_CONSOLE)
+#include <stdio.h>
+#define PRINT       printf
+#else
+#include <misc/printk.h>
+#define PRINT       printk
+#endif
+
+/**
+ * Jerryscript simple test loop
+ */
+int jerryscript_test ()
+{
+  jerry_value_t ret_val;
+
+  const char script[] =
+  "var test=0; " \
+  "for (var t=100; t<1000; t++) test+=t; " \
+  "print ('Hi JS World! '+test);";
+
+  printf ("Script [%s]\n", script);
+  ret_val = jerry_eval ((jerry_char_t *) script,
+      strlen (script),
+      false);
+
+  return jerry_value_has_error_flag (ret_val) ? -1 : 0;
+} /* jerryscript_test */
+
+#ifdef CONFIG_USE_JS_SHELL
+#define VERBOSE 0x01
+
+static char *source_buffer = NULL;
+static unsigned char flags = 0;
+
+static int shell_cmd_verbose (int argc, char *argv[])
+{
+  printf ("Enable verbose \n");
+  flags |= VERBOSE;
+  return 0;
+} /* shell_cmd_verbose */
+
+
+static int shell_cmd_syntax_help (int argc, char *argv[])
+{
+  printf ("version jerryscript & zephyr versions\n");
+  return 0;
+} /* shell_cmd_syntax_help */
+
+
+static int shell_cmd_version (int argc, char *argv[])
+{
+  uint32_t version = sys_kernel_version_get ();
+
+  printf ("Jerryscript API %d.%d\n", JERRY_API_MAJOR_VERSION, JERRY_API_MINOR_VERSION);
+
+  printk ("Zephyr version %d.%d.%d\n", SYS_KERNEL_VER_MAJOR (version),
+    SYS_KERNEL_VER_MINOR (version),
+    SYS_KERNEL_VER_PATCHLEVEL (version));
+  return 0;
+} /* shell_cmd_version */
+
+
+static int shell_cmd_test (int argc, char *argv[])
+{
+  return jerryscript_test ();
+} /* shell_cmd_test */
+
+static int shell_cmd_handler (int argc, char *argv[])
+{
+  if (argc <= 0)
+  {
+    return -1;
+  }
+
+  unsigned int size = 0;
+  for (int t = 0; t < argc; t++)
+  {
+    size += strlen (argv[t]) + 1;
+  }
+
+  source_buffer = (char *) malloc (size);
+
+  char *d = source_buffer;
+  unsigned int len;
+
+  for (int t = 0; t < argc; t++)
+  {
+    len = strlen (argv[t]);
+    memcpy (d, argv[t], len);
+    d += len;
+    *d = ' ';
+    d++;
+  }
+
+  * (d - 1) = '\0';
+
+  if (flags & VERBOSE)
+  {
+    printf ("[%s] %lu\n", source_buffer, strlen (source_buffer));
+  }
+
+  jerry_value_t ret_val;
+
+  ret_val = jerry_eval ((jerry_char_t *) source_buffer,
+    strlen (source_buffer),
+    false);
+
+  free (source_buffer);
+
+  if (jerry_value_has_error_flag (ret_val))
+  {
+    printf ("Failed to run JS\n");
+  }
+
+  jerry_release_value (ret_val);
+
+  return 0;
+} /* shell_cmd_handler */
+
+#define SHELL_COMMAND(name,cmd) { name, cmd }
+
+const struct shell_cmd commands[] =
+{
+  SHELL_COMMAND ("syntax", shell_cmd_syntax_help),
+  SHELL_COMMAND ("version", shell_cmd_version),
+  SHELL_COMMAND ("test", shell_cmd_test),
+  SHELL_COMMAND ("verbose", shell_cmd_verbose),
+  SHELL_COMMAND (NULL, NULL)
+};
+#endif
+
+void main (void)
+{
+#ifdef CONFIG_USE_JS_SHELL
+  jerry_init (JERRY_INIT_EMPTY);
+  printf("Jerry Shell " __DATE__ " " __TIME__ "\n");
+  shell_register_app_cmd_handler (shell_cmd_handler);
+  shell_init ("js> ", commands);
+  /* Don't call jerry_cleanup() here, as shell_init() returns after setting
+     up background task to process shell input, and that task calls
+     shell_cmd_handler(), etc. as callbacks. This processing happens in
+     the infinite loop, so JerryScript doesn't need to be de-initialized. */
+#else
+  printf("Jerry Uploader " __DATE__ " " __TIME__ "\n");
+  uart_uploader_init();
+#endif
+} /* main */
+
